866e416ad0c934374ab345e7df285a3228d4b215
iddqd0 <iddqd0@users.noreply.github.com>
2023-05-11 02:06:01 +0300

    Update entrypoint.sh
    Change `eval` to `exec`
    
    fix parkervcp/images#73

--- games/source/entrypoint.sh
+++ games/source/entrypoint.sh
@@ -70,4 +70,4 @@ MODIFIED_STARTUP=$(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')
 echo -e ":/home/container$ ${MODIFIED_STARTUP}"
 
 # Run the Server
-eval ${MODIFIED_STARTUP}
+exec ${MODIFIED_STARTUP}
