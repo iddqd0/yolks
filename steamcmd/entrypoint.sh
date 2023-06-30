6aba2873f2ecfe8781ede9276db8479590f84e73
iddqd0 <iddqd0@users.noreply.github.com>
2023-06-30 16:24:10 +0300

    Update entrypoint.sh

--- steamcmd/entrypoint.sh
+++ steamcmd/entrypoint.sh
@@ -84,4 +84,4 @@ MODIFIED_STARTUP=$(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')
 echo -e ":/home/container$ ${MODIFIED_STARTUP}"
 
 # Run the Server
-eval ${MODIFIED_STARTUP}
+exec env ${MODIFIED_STARTUP}
